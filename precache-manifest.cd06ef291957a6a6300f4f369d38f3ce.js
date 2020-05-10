8a0a3a4cc84dd8e3d544e8aa8b84710c71842717
Jay_Kim <dkel03@naver.com>
2020-05-10 22:52:41 +0900

    Updates

---
+++ precache-manifest.cd06ef291957a6a6300f4f369d38f3ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4d20a2b819d3aee349ee08a59162350",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "91209873d4db2046e99c",
+    "url": "/corona/static/js/2.0664e649.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/corona/static/js/2.0664e649.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10d1471e9c57b092589c",
+    "url": "/corona/static/js/main.7f37584e.chunk.js"
+  },
+  {
+    "revision": "b711712299ab2066e5bf",
+    "url": "/corona/static/js/runtime-main.c1a05405.js"
+  }
+]);
