495147f0ae6a04774788686fed05c48447ab9c5c
Jay_Kim <dkel03@naver.com>
2020-05-11 18:48:17 +0900

    Updates

--- precache-manifest.94dc4b39574d47e3b56fffb580c86648.js
+++ precache-manifest.94dc4b39574d47e3b56fffb580c86648.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3aa7d10d3ebf040681275cf85b970371",
+    "revision": "10b34087394bdd8249d2aed46ced64bb",
     "url": "/corona/index.html"
   },
   {
-    "revision": "9a51f438091f55e1d1a8",
+    "revision": "094fb2406b8a78e44ade",
     "url": "/corona/static/css/main.69a85fe1.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/corona/static/js/2.30d78f7c.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "9a51f438091f55e1d1a8",
-    "url": "/corona/static/js/main.e7c047e6.chunk.js"
+    "revision": "094fb2406b8a78e44ade",
+    "url": "/corona/static/js/main.40391bb6.chunk.js"
   },
   {
     "revision": "b711712299ab2066e5bf",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corona/precache-manifest.fdfb324f3dd3fa87820276bc1c55e566.js"
+  "/corona/precache-manifest.94dc4b39574d47e3b56fffb580c86648.js"
 );
 
 self.addEventListener('message', (event) => {
